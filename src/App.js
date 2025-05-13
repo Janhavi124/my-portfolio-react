ea6e63cc0fb83e45c8e5a5f8ca542fc56ce7921d
Janhavi Potdar <janhavi.potdar@Agilarc.com>
2025-05-13 16:29:46 -0400

    Update styles: added red background to body

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Hello, I'm Janhavi Potdar</h1>
+      <h1>Hola, I'm Janhavi Potdar</h1>
       <p>This is my portfolio built with React!</p>
     </div>
   );
